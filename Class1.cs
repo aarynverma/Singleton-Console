669a17303846463893d707ce90a271e087e70039
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2022-08-01 18:00:51 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;  
+  
+namespace Singleton  
+{  
+  class Singleton  
+  {  
+    static int instanceCounter = 0;  
+    public Singleton()  
+    {  
+      instanceCounter++;  
+      WriteLine("Instances Increment " + instanceCounter);
+
+      instanceCounter--;
+      WriteLine("Instances Decrement " + instanceCounter);
+    }  
+  }  
+}  
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             new Singleton();
+            ReadLine();
+        }
+    }
+}
+
